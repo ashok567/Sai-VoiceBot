c9cc1c49bf5dcc61163fb60485db59802ebe8f60
Ashok Thakur <iashok180@gmail.com>
2018-08-31 02:13:26 +0530

    modified bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 from chatterbot import ChatBot
 from gtts import gTTS
 import speech_recognition as sr
+import wikipedia as wiki
 import os
+import re
+import webbrowser
+import urllib.request
+from urllib.parse import urlencode
 
 bot = ChatBot('Bot')
+flag=True
 
 def audio_process(audio):
 	tts = gTTS(text=audio, lang='en')
-	tts.save("chat.mp3")
+	tts.save("audio.mp3")
 
-	os.system("mpg321 chat.mp3")
+	os.system("mpg321 audio.mp3")
 
 def chat_listen():
 	r =sr.Recognizer()
@@ -30,17 +36,63 @@ def chat_listen():
 
 	
 def chat_bot(command):
-	if command in ['bye', 'exit', 'quit', 'terminate', 'stop']:
+	if 'google' in command:
+		audio_process('What should I search')
+		query = chat_listen()
+		url = "https://www.google.co.in/search?q=" +(str(query))+ "&oq="+(str(query))+"&gs_l=serp.12..0i71l8.0.0.0.6391.0.0.0.0.0.0.0.0..0.0....0...1c..64.serp..0.0.0.UiQhpfaBsuU"
+		webbrowser.open_new(url)
+		
+	elif 'website' in command:
+		audio_process('tell me the name')
+		web_name = chat_listen()
+		reg_ex = re.search('(.+)', web_name)
+		if reg_ex:
+			domain = reg_ex.group(1)
+			url = 'https://www.' + domain
+			print("opening "+ url)
+			webbrowser.open(url)
+
+	elif 'youtube' in command:
+		audio_process('tell me the video name')
+		video_name = chat_listen()
+		query_string = urlencode({"query" : video_name})
+		html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+		search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+		result = "http://www.youtube.com/watch?v=" + search_results[0]
+		print("opening "+ result)
+		webbrowser.open_new(result)
+
+	elif 'wiki' in command or 'wikipedia' in command:
+		audio_process('tell me you query')
+		query = chat_listen()
+		try:
+			audio_process("Processing...will take some time")
+			audio_process(wiki.summary(query))
+			
+		except Exception as e:
+			print(e)
+			audio_process("I dont know about " + query)
+			audio_process("Please come again")
+
+	elif command in ['bye', 'exit', 'quit', 'terminate', 'stop']:
 		print("Sai: Bye...See you")
 		audio_process('Bye, see you')
 		exit()
+
 	else:
 		reply = bot.get_response(command)
 		print("Sai:", reply)
 		audio_process(str(reply))
 		
 
-
 audio_process("Hey there, How may I help you")
-while True:
-	chat_bot(chat_listen())
+chat_bot(chat_listen())
+while flag==True:
+	audio_process("You want more help?, Please say")
+	ans = chat_listen()
+	if 'yes' in ans:
+		audio_process("how may I assist you")
+		chat_bot(chat_listen())
+	else:
+		audio_process("Bye, see you")
+		flag=False
